ff6d9798ae4cfa15b850b9356cfd720a33edf718
Pocsai Norbert <pocsainorbertjanos@gmail.com>
2023-05-24 03:38:48 +0200

    MinorMapAndTankChanges

--- Armored Rampage/Assets/Scripts/GameOverScreen.cs
+++ Armored Rampage/Assets/Scripts/GameOverScreen.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class GameOverScreen : MonoBehaviour
 {
- 
+
 
     public void Setup()
     {
         gameObject.SetActive(true);
     }
 
+    
 
     public void RestartButton()
     {
